48acc72993dc8cb896d57a245e0613936d282447
Adam Anderson <adama@olo.com>
2017-10-19 17:26:33 -0400

    Vendor-specific SQL

--- Hackathon.Rezoom/Program.fs
+++ Hackathon.Rezoom/Program.fs
@@ -22,6 +22,13 @@ type selectPersonByName = SQL<"""
     where name = @name
 """>
 
+//type selectPersonChecksum = SQL<"select *, checksum(*) as csum from person">
+type selectPersonChecksum = SQL<"vendor tsql {
+        select *, checksum(*) as csum from person
+    } imagine {
+        select *, cast(0 as int) as csum from person
+    }">
+
 let migrate() =
     // customize the default migration config so that it outputs a message after running a migration
     let config =
@@ -35,7 +42,7 @@ let migrate() =
 let populatePerson() =
     
     plan {
-         for name in ["Adam";"Ben";"Michael";"Sam"] do
+         for name in batch ["Adam";"Ben";"Michael";"Sam"] do
             do! insertPerson.Command(name).Plan()
          }
 
@@ -43,14 +50,18 @@ let populatePerson() =
 let main argv =
     migrate()
     stdout.WriteLine("Done running migrations")
-    let (adams,bens) = (
-        plan {
-            let! adams,bens = selectPersonByName.Command("Adam").Plan(),selectPersonByName.Command("Ben").Plan()
-            return (adams,bens)
-        }
-        |> execute ExecutionConfig.Default).Result
-    Seq.iter2 (fun (a:selectPersonByName.Row) (b:selectPersonByName.Row) -> printfn "%s %s" a.name b.name ) adams bens    
-    stdout.WriteLine("Done populating")
-    // stdin.ReadLine() |> ignore
-    // return 0 status code
+    //populatePerson() |> execute ExecutionConfig.Default |> ignore
+    //stdout.WriteLine("Done populating")
+    //let (adams,bens,sams) = (
+    //    plan {
+    //        let! adams,bens = selectPersonByName.Command("Adam").Plan(),selectPersonByName.Command("Ben").Plan()
+    //        let! adams2,sams = selectPersonByName.Command("Adam").Plan(),selectPersonByName.Command("Sam").Plan()
+    //        let! michaels,michaels2 = selectPersonByName.Command("Michael").Plan(),selectPersonByName.Command("Michael").Plan()
+    //        let! adams3,adams4 = selectPersonByName.Command("Adam").Plan(),selectPersonByName.Command("Adam").Plan()
+    //        return (adams,bens,sams)
+    //    }
+    //    |> execute ExecutionConfig.Default).Result
+    //Seq.iter2 (fun (a:selectPersonByName.Row) (b:selectPersonByName.Row) -> printfn "%s %s" a.name b.name ) adams bens    
+    selectPersonChecksum.Command().Execute(new ConnectionContext())
+    |> Seq.iter (fun (row:selectPersonChecksum.Row) -> printfn "%d %s %d" row.id row.name row.csum)
     0
